5fa42d479307d4e8e89c72a5b092a42ccf2bc1ef
vineetgoyal2001 <vineetgoyal2001@gmail.com>
2025-08-07 21:32:15 +0100

    first change

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -118,8 +118,7 @@ test('@Child windows hadl', async ({browser})=>
   
    await page.locator("[routerlink*='cart']").click();
    
- 
-   await page.locator("div li").first().waitFor();
+    await page.locator("div li").first().waitFor();
    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
    expect(bool).toBeTruthy();
    await page.locator("text=Checkout").click();
